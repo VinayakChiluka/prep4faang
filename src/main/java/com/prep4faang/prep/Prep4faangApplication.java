9ff29051093e66c8f11542c1a9b40b2cf89bf2cd
Vinayak.Chiluka <chilukavinayak.p@gmail.com>
2025-05-02 09:09:24 +0530

    Initial commit

---
+++ src/main/java/com/prep4faang/prep/Prep4faangApplication.java
@@ -0,0 +1,13 @@
+package com.prep4faang.prep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prep4faangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prep4faangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prep4faang/prep/Prep4faangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prep4faang.prep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prep4faangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
